cd09f3ce0b507155ccd7a636ab7cf0695363596e
hung <hung@studyguide.dev>
2025-08-29 09:24:33 +0700

    fix bug

--- src/messengers/messengers.module.ts
+++ src/messengers/messengers.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MessengersController } from './messengers.controller';
 import { MessengersService } from './messengers.service';
+import { QueueModule } from 'src/queue/queue.module';
 
 @Module({
   controllers: [MessengersController],
   providers: [MessengersService],
+  imports: [QueueModule]
 })
 export class MessengersModule {}
